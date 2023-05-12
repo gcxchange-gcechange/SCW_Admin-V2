f9aaa28c6437c3ae37fad99524818ed5d3f90e36
gabmor38 <gabriela.morenor@gmail.com>
2023-05-12 17:43:40 -0400

    process of redesigning the form layout

--- src/webparts/scwAdmin/components/ItemFormDetails.tsx
+++ src/webparts/scwAdmin/components/ItemFormDetails.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { TextField } from 'office-ui-fabric-react/lib/TextField';
+import { ITextFieldStyles, TextField } from 'office-ui-fabric-react/lib/TextField';
 import * as React from 'react';
 import { IScwAdminState } from './IScwAdminState';
-
+import { IStackProps, IStackStyles,Stack } from 'office-ui-fabric-react';
 
 
 
@@ -11,22 +11,39 @@ const ItemFormDetails: React.FunctionComponent<IScwAdminState> = (props) => {
 
     const { selectedRowData } = props;
 
+    // const stackTokens = { childrenGap: 18 }
+
+    const stackTokens = { childrenGap: 50 };
+    const stackStyles: Partial<IStackStyles> = { root: { width: 800} };
+    const columnProps: Partial<IStackProps> = {
+    tokens: { childrenGap: 10 },
+    styles: { root: { width: '100%' } },
+    };
+
+    const textFieldWrapper: Partial<ITextFieldStyles> =  {
+        wrapper: { borderBottom: 'none', }
+    }
+
     // console.log("ItemPROPS", selectedRowData);
-
-
-
 return (
     <>
         
-        <h2>Request Detail</h2>
+        <h2>Community creation request detail</h2>
+        <Stack horizontal tokens={stackTokens} styles={stackStyles}>
+            <Stack {...columnProps}>
+                <TextField label="Request Id:" styles= {textFieldWrapper} underlined readOnly defaultValue={selectedRowData.ID} />
+                <TextField label="Status:" styles= {textFieldWrapper} underlined readOnly defaultValue={selectedRowData.status} />
+                <TextField label="Requester Email:" styles= {textFieldWrapper} underlined readOnly defaultValue={selectedRowData.requesterEmail} />
+                <TextField label="Community Sharepoint Url:" styles= {textFieldWrapper} underlined readOnly defaultValue={selectedRowData.siteUrl} />
+            </Stack>
+        </Stack>
+        <hr/>
         <div>
-            <TextField label="Space Name (EN)" readOnly defaultValue={selectedRowData.spaceName} multiline autoAdjustHeight/>
-            <TextField label="Space Name (FR)" readOnly defaultValue={selectedRowData.spaceName} multiline autoAdjustHeight/>         
-            <TextField label="Space Description (EN)" readOnly defaultValue={selectedRowData.spaceDescription} multiline autoAdjustHeight/>
-            <TextField label="Space Description (FR)" readOnly defaultValue={selectedRowData.spaceDescriptionFR} multiline autoAdjustHeight/>
-            <TextField label="Team Purpose" readOnly defaultValue={ selectedRowData.businessJustification }  multiline autoAdjustHeight/>
-            <TextField label="SharePoint Site url" readOnly defaultValue={ selectedRowData.siteUrl} />
-            <TextField label="Requester Email" readOnly defaultValue={ selectedRowData.requesterEmail} />   
+            <TextField label="Community Purpose" readOnly defaultValue={ selectedRowData.businessJustification }  multiline autoAdjustHeight/>
+            <TextField label="English Community Name" readOnly defaultValue={selectedRowData.spaceName} multiline autoAdjustHeight/>
+            <TextField label="French Community Name" readOnly defaultValue={selectedRowData.spaceName} multiline autoAdjustHeight/>         
+            <TextField label="English description" readOnly defaultValue={selectedRowData.spaceDescription} multiline autoAdjustHeight/>
+            <TextField label="French description" readOnly defaultValue={selectedRowData.spaceDescriptionFR} multiline autoAdjustHeight/>
         </div>
         
     </>
