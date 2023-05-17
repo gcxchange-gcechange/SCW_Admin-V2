47436809abc42ca5726942d5c56ebfc01b74d38e
gabmor38 <gabriela.morenor@gmail.com>
2023-05-17 17:18:19 -0400

    changed layout

--- src/webparts/scwAdmin/components/ItemFormDetails.tsx
+++ src/webparts/scwAdmin/components/ItemFormDetails.tsx
@@ -10,8 +10,7 @@ import { PeoplePicker, PrincipalType } from "@pnp/spfx-controls-react/lib/People
 const ItemFormDetails: React.FunctionComponent<IScwAdminState> = (props) => {
 
     console.log(props)
-   
-
+    
     const { selectedRowData } = props;
 
     const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>):void  => {
@@ -26,6 +25,11 @@ const ItemFormDetails: React.FunctionComponent<IScwAdminState> = (props) => {
              
     }
 
+    const onSelectedKey = ( event: React.ChangeEvent<HTMLInputElement>, option: IChoiceGroupOption):void => {
+
+        props.decisionChoiceCallback(option.key)
+        
+    }
     
 
     const stackStyles: Partial<IStackStyles> = { root: { width: 800} };
@@ -37,7 +41,8 @@ const ItemFormDetails: React.FunctionComponent<IScwAdminState> = (props) => {
 
 
     const textFieldBackground: Partial<ITextFieldStyles> = {
-        field: { backgroundColor: 'rgb(218, 218, 218, 0.29)'}
+        field: {backgroundColor: 'rgb(218, 218, 218, 0.29)'},
+        fieldGroup: {borderColor: 'rgb(218, 218, 218, 0.29)'}
         // subComponentStyles: { label: {color:  '#e8e8e8'}}
     }
 
@@ -45,14 +50,15 @@ const ItemFormDetails: React.FunctionComponent<IScwAdminState> = (props) => {
     const customFieldStyles = mergeStyleSets ({
         wrapper: { borderBottom: 'none', outline: 'transparent'},
         field: { borderBottom: 'none'},
-        fieldGroup:{ borderBottom: 'transparent', boxShadow: 'none', outline:'transparent'},
-        subComponentStyles: { label: {root: { width: '190px'}}},
-        prefix: {font: '18px', paddingRight: '0px', paddingTop: '6px'}
+        fieldGroup:{ borderColor: 'none', boxShadow: 'none', outlineColor:'transparent'},
+        subComponentStyles: { label: {root: { width: '190px', borderBottom: 'none'}}},
+        prefix: {font: '18px', paddingRight: '0px', paddingTop: '6px', background: 'none'}
 
       });
     
 
     const renderIcon = (): any => {
+
 
         if (selectedRowData.status === 'Submitted') {
             return <Icon style={{color: '#F7B80A'}} iconName='AlertSolid'/>
@@ -75,12 +81,7 @@ const ItemFormDetails: React.FunctionComponent<IScwAdminState> = (props) => {
         { key: 'B', text: 'Reject community creation' },
       ];
 
-    // const labelFontStyle: Partial<ILabelStyles> = {
-    //     root:{ fontSize: '18px'},
-        
-    // }
-
-    // console.log("ItemPROPS", selectedRowData);
+   
 
 
     return (
@@ -90,19 +91,18 @@ const ItemFormDetails: React.FunctionComponent<IScwAdminState> = (props) => {
             <div>
                 <Stack horizontal styles={stackStyles}>
                     <Stack  {...columnProps}>
-                        <TextField label="Request Id:" styles= {customFieldStyles} underlined readOnly defaultValue={selectedRowData.id} />
+                        <TextField label="Request id:" styles= {customFieldStyles} underlined readOnly defaultValue={selectedRowData.id} />
                         <TextField label="Status:" styles= {customFieldStyles} underlined readOnly prefix={renderIcon()} defaultValue={selectedRowData.status}/>
-                        <TextField label="Requester Email:" styles= {customFieldStyles} underlined readOnly defaultValue={selectedRowData.requesterEmail} />
-                        <TextField label="Community Sharepoint Url:" styles= {customFieldStyles} underlined readOnly defaultValue={selectedRowData.siteUrl} />
-                        
+                        <TextField label="Requester email:" styles= {customFieldStyles} underlined readOnly defaultValue={selectedRowData.requesterEmail} />
+                        <TextField label="Community sharepoint url:" styles= {customFieldStyles} underlined readOnly defaultValue={selectedRowData.siteUrl} />
                     </Stack>
                 </Stack>
             </div>
              
             <div className={styles.formContainer}>
-                <TextField styles={textFieldBackground} label="Community Purpose" readOnly disabled defaultValue={ selectedRowData.businessJustification } multiline rows={3}/>
-                <TextField styles={textFieldBackground} label="English Community Name" readOnly defaultValue={selectedRowData.spaceName} multiline rows={2} />
-                <TextField styles={textFieldBackground} label="French Community Name" readOnly defaultValue={selectedRowData.spaceName} multiline rows={2}  />         
+                <TextField styles={textFieldBackground} label="Community purpose" readOnly defaultValue={ selectedRowData.businessJustification } rows={3}/>
+                <TextField styles={textFieldBackground} label="English community name" readOnly defaultValue={selectedRowData.spaceName} rows={2} />
+                <TextField styles={textFieldBackground} label="French community name" readOnly defaultValue={selectedRowData.spaceName}  rows={2}  />         
                 <TextField styles={textFieldBackground} label="English description" readOnly defaultValue={selectedRowData.spaceDescription} />
                 <TextField styles={textFieldBackground} label="French description" readOnly defaultValue={selectedRowData.spaceDescriptionFR} />
                 <PeoplePicker
@@ -116,26 +116,23 @@ const ItemFormDetails: React.FunctionComponent<IScwAdminState> = (props) => {
                     principalTypes={[PrincipalType.User]}
                 />
             </div>
-
-            <div style={{paddingBottom: '18px'}}>
-                <Stack horizontal verticalAlign='center'>
-                    <span className={styles.asteriks}>&#42;</span>
-                    <h3>Community creation decision</h3>
-                </Stack>
-                        <ChoiceGroup id='choiceDecision' options={decisionOptions} />
-                                 
-            </div>
-
-            <div>
-                <TextField label="Comment (optional)" placeholder= "Type a comment to send to the requestor" multiline autoAdjustHeight onChange={handleOnChange} defaultValue={props.selectedRowData.comment}/>
-            </div>
-            
-             
-            {/* { selectedRowData.status === 'Submitted' ?
-                <hr/>
-                :
-                null
-            } */}
+            {   selectedRowData.status === 'Submitted' &&
+                <>
+                    <div style={{paddingBottom: '18px'}}>
+                    <Stack horizontal verticalAlign='center'>
+                        <span className={styles.asteriks}>&#42;</span>
+                        <h3>Community creation decision</h3>
+                    </Stack>
+                    <ChoiceGroup id='choiceDecision' options={decisionOptions} onChange={onSelectedKey}/>
+                                    
+                    </div>
+ 
+                    <div>
+                        <TextField label="Decision comments (optional)" placeholder= "Type a comment to send to the requestor" multiline autoAdjustHeight onChange={handleOnChange} defaultValue={props.selectedRowData.comment}/>
+                    </div>
+                </>      
+            }
+           
             
         </>
     )
