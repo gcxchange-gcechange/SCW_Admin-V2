9faa695e1a2a58be74bc7d0bb42524b4bf254377
gabmor38 <gabriela.morenor@gmail.com>
2023-05-24 16:03:00 -0400

    commented out interface

--- src/webparts/scwAdmin/components/IScwAdminState.ts
+++ src/webparts/scwAdmin/components/IScwAdminState.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { WebPartContext } from "@microsoft/sp-webpart-base";
+// import { WebPartContext } from "@microsoft/sp-webpart-base";
 
 
 
-export interface IScwAdminState {
-    context?: WebPartContext;
-    selectedRowData: any;
-    confirmationComments?:(value: string) => void;
-    decisionChoiceCallback?:(option: string) => void;
+// export interface IScwAdminState {
+//     requestList: any[];
+//     context?: WebPartContext;
+//     selectedRowData: any;
+//     confirmationComments?:(value: string) => void;
+//     decisionChoiceCallback?:(option: string) => void;
 
-}
+// }
